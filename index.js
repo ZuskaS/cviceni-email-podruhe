06bc3e2f0fcd11fa672210709e277e7999d1f336
Zuzana Ilečková <yes>
2025-04-04 21:51:01 +0200

    úprava fillBody tak, aby měla třetí parametr funkci, která bude upravovat styl rozloučení, následně úprava celého body i oslovení

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const goodbye = (name) => {
   return 'S pozdravem ' + name;
 };
 
+const formalGoodbye = (name) => {
+  return 'S ucitivým pozdravem ' + name;
+};
+
+const rudeGoodbye = (name) => {
+  return 'Tě pic ' + '<br>' + name;
+};
+
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML = body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = goodbyeFunction;
+  name = 'Dobromyslík';
 };
+
+fillSubject('Mávám');
+fillBody(
+  'Túze tě zdravím, jsme se dlouho neviděli a fakt by ses měl ozvat!' +
+    '<br>' +
+    'Dobromyslík' +
+    '<br>' +
+    rudeGoodbye(name),
+);
+
+const closingElm = document.querySelector('.email__greeting');
+closingElm.textContent = ' ';
